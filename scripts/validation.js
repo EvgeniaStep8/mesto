3940c5b14601a14c9db2ea46300094924c6fa7c7
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-02-09 20:48:55 +0300

    feat: добавить неактивное состояние для submit при валидации

--- scripts/validation.js
+++ scripts/validation.js
@@ -13,22 +13,41 @@ function hideInputError(formElement, inputElement) {
 }
 
 function checkInputValidity (formElement, inputElement) {
-  const hasInputElementInvalid = !inputElement.validity.valid;
-    if (hasInputElementInvalid) {
+    if (!inputElement.validity.valid) {
       showInputError(formElement, inputElement, inputElement.validationMessage);
     } else {
       hideInputError(formElement, inputElement);
     }
 }
 
+function hasInvalidInput(inputList) {
+  return inputList.some( (inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleButtonState(formElement, inputList) {
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-button_disabled');
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove('popup__save-button_disabled');
+    buttonElement.disabled = false;
+  }
+}
+
 function addInputListeners (formElement) {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  toggleButtonState(formElement, inputList);
   inputList.forEach ((inputElement) => {
     inputElement.addEventListener('input', () => {
     checkInputValidity (formElement, inputElement);
+    toggleButtonState(formElement, inputList)
     });
   });
 }
+  
 
 function enableValidation() {
   const formList = Array.from(document.querySelectorAll('.popup__form'));
