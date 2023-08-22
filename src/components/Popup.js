92f3f4cc958588e844269d91d21b91c06bc7c4ae
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-08-22 18:52:44 +0300

    fix pixel perfect

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,7 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
-    this._handleEscUp = this._handleEscUp.bind(this);
+    this._handleEscUp.bind(this);
   }
 
   open() {
